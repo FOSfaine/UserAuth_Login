e9593df1a6494f844bb7abaeadffae8bdf2e4d35
Esther Cho <esthercho@Esthers-MacBook-Air.local>
2019-09-30 12:55:40 -0600

    added basic code

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var Sequelize= require('sequelize');
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+//Middleware EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 3306;
+
+app.listen(PORT, console.log('Server started on port ${PORT}'));
+
+
+var basename = path.basename(module.filename);
+
+var env = process.env.NODE_ENV || "development";
+
+//DB Config
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
+
+// if (config.use_env_variable) {
+//     var sequelize = new Sequelize(process.env[config.use_env_variable]);
+//   } else {
+//     var sequelize = new Sequelize(config.database, config.username, config.password, config);
+//   }
+
+//   fs
+//   .readdirSync(__dirname)
+//   .filter(function(file) {
+//     return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+//   })
+//   .forEach(function(file) {
+//     var model = sequelize["import"](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(function(modelName) {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+
+module.exports = db;
